87f220913972dcfae91bbb2fcee606de3b65fa94
cecile hirschauer <cecile.h.75@gmail.com>
2023-02-20 14:53:20 +0100

    Run invoicer

--- src/main/java/fr/cecile75/invoicerdemop3/InvoicerDemoP3Application.java
+++ src/main/java/fr/cecile75/invoicerdemop3/InvoicerDemoP3Application.java
@@ -1,12 +1,25 @@
 package fr.cecile75.invoicerdemop3;
 
+import fr.cecile75.invoicerdemop3.entity.Invoice;
+import fr.cecile75.invoicerdemop3.service.InvoiceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 public class InvoicerDemoP3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(InvoicerDemoP3Application.class, args);
+
+        //SpringApplication.run(InvoicerDemoP3Application.class, arg);
+
+        System.out.println("Customer name");
+        Scanner sc = new Scanner(System.in);
+        String customerName = sc.nextLine();
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(customerName);
+        InvoiceService invoiceService = new InvoiceService();
+        invoiceService.createInvoice(invoice);
     }
 
 }
